a5c46b2fe2abe79daffaefab5e998f0050d8ca3a
Karan <mkarandeep.1111@gmail.com>
2026-05-06 20:15:24 +0530

    Update lib/rex/arch.rb
    Co-authored-by: Spencer McIntyre <58950994+smcintyre-r7@users.noreply.github.com>

--- lib/rex/arch.rb
+++ lib/rex/arch.rb
@@ -220,7 +220,7 @@ module Arch
   #
   def self.from_uname(uname_arch)
     case uname_arch.to_s.strip
-    when 'x86_64', 'amd64'
+    when 'x86_64', 'x64', 'amd64'
       ARCH_X86_64
     when 'i686', 'i386', 'i486', 'i586'
       ARCH_X86
